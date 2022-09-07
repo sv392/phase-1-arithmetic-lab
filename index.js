bd7c41821db4128f6960a1d54c60913d63d1293b
Sanh Vu <sanhvu020@gmail.com>
2022-09-06 21:52:15 -0400

    commit tree be6170a20f007a2c034729555d2cd5873dd5f4c4

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+    let multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+let num3 = 29;
+let num4 = 5;
+    const mod = num3 % num4;
+
+const max = Math.max(1,2,3,4,5,6,7,20);
